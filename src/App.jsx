1fec17e4a3a2bb80e8704314e735b01af23c6f7e
Michal <michal.erik.czajkowski@gmail.com>
2023-07-10 00:57:54 +0200

    feat: create counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import "./App.css";
+import './App.css';
+import { Text } from './components/Text';
+import { Counter } from './components/counter';
 
 function App() {
-  return <div>Lights generator</div>;
+  return (
+    <div>
+      <nav className='nav'>
+        <Text>0 bulbs</Text>
+        <Counter text='rows' count='0' />
+        <Counter text='columns' count='0' />
+      </nav>
+    </div>
+  );
 }
 
 export default App;
